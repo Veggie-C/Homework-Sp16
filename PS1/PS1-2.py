233b6e03a942fe80bceca7a4da568236d4044059
Peter Iordanov <iordano2@illinois.edu>
2016-01-25 19:35:50 -0600

    tweaking introduction

--- PS1/PS1-2.py
+++ PS1/PS1-2.py
@@ -2,9 +2,14 @@
 this function takes in three parameters a, b, and c
 and returns the solution of x for 0 = a(x^2) + bx + c
 print out both solutions to the equation
+For example quadratic(5,6,1) will return
+-5.6
+-6.4
+
 you can assume the discriminant of the function will be greater than 0
 and that it will have two solutions
 Hint: How can you represent a square root in the form of a power?
+or maybe there is a square root function already available to you...
 """
 
 
