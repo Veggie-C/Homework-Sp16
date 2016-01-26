856cead8d6412a9231814438c9fe2c1e04866257
Peter Iordanov <iordano2@illinois.edu>
2016-01-25 18:52:24 -0600

    added PS2

---
+++ PS1/PS1-2.py
@@ -0,0 +1,12 @@
+"""
+this function takes in three parameters a, b, and c
+and returns the solution of x for 0 = a(x^2) + bx + c
+print out both solutions to the equation
+you can assume the discriminant of the function will be greater than 0
+and that it will have two solutions
+Hint: How can you represent a square root in the form of a power?
+"""
+
+
+def quadratic(a,b,c):
+    #PLACE YOUR CODE HERE
