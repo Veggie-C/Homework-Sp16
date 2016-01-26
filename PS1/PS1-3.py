d79a9b3929e91506da67bf5182f3f102ce3a3f23
Peter Iordanov <iordano2@illinois.edu>
2016-01-25 20:01:56 -0600

    add problem 3

---
+++ PS1/PS1-3.py
@@ -0,0 +1,9 @@
+"""
+this function takes in four parameters
+x1, y1, x2, y2
+and returns the slope of the line that passes through points (x1,y1) and (x2,y2)
+
+"""
+
+def slope(x1,y1,x2,y2):
+    #PLACE YOUR CODE HERE
